69c481a7669914e0dff6aece3d7d821b23d23fdc
Agustin Feuerhake <agustin@platan.us>
2013-07-06 13:38:00 -0400

    adds http post

--- get_handler.py
+++ get_handler.py
@@ -1,27 +1,30 @@
 from BaseHTTPServer import BaseHTTPRequestHandler
+import os
 import urlparse
+import url_validator
 
 class GetHandler(BaseHTTPRequestHandler):
 
 	def do_GET(self):
 		parsed_path = urlparse.urlparse(self.path)
-		message = '\n'.join([
-		'CLIENT VALUES:',
-		'client_address=%s (%s)' % (self.client_address,
-		self.address_string()),
-		'command=%s' % self.command,
-		'path=%s' % self.path,
-		'real path=%s' % parsed_path.path,
-		'query=%s' % parsed_path.query,
-		'request_version=%s' % self.request_version,
-		'',
-		'SERVER VALUES:',
-		'server_version=%s' % self.server_version,
-		'sys_version=%s' % self.sys_version,
-		'protocol_version=%s' % self.protocol_version,
-		'',
-		]) 
+		message = '<html><body><form method="POST"> <input type="text" name="url"/></form><body></html>' 
 		self.send_response(200)
 		self.end_headers()
 		self.wfile.write(message)
+		return
+	def do_POST(self):
+		execute = None
+		length = int(self.headers['Content-Length'])
+		post_data = urlparse.parse_qs(self.rfile.read(length).decode('utf-8'))
+		for key, value in post_data.iteritems():
+			if(key=="url" and value[0] and url_validator.validate(value[0])):
+				execute = 'youtube-dl ' + value[0]
+		self.send_response(200)
+		self.end_headers()
+		if (execute):
+			os.system(execute)
+			os.system('mv *.mp4 /@volume1/')
+			self.wfile.write("executed " + execute)
+		else:
+			self.wfile.write("error")
 		return

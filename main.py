fbe0f8ecc2d9b1b067acf07668f68d0e39d64ffd
Agustin Feuerhake <agustin@platan.us>
2013-07-06 13:56:07 -0400

    binds to external ips

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from get_handler import *
 if __name__ == '__main__':
 	from BaseHTTPServer import HTTPServer
-	server = HTTPServer(('localhost', 5454), GetHandler)
+	server = HTTPServer(('0.0.0.0', 5454), GetHandler)
 	print 'Starting server, use  to stop'
 	server.serve_forever()
